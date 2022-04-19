fb11c67dd1ac0598bd08216f26c98910d799ed70
Alex Pritchard <pritchard.alex@gmail.com>
2022-04-19 10:22:25 -0500

    Changed proxy destination name

--- solvle-front/src/setupProxy.js
+++ solvle-front/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function(app) {
     app.use(createProxyMiddleware('/solvle',
-        { target: 'http://solvleback:8081',
+        { target: 'http://server:8081',
             changeOrigin: true
         }
         ));
