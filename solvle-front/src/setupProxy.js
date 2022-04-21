488533657f6bc7b15f607c0bec862bdff9e6310d
Alex Pritchard <pritchard.alex@gmail.com>
2022-04-21 14:22:05 -0500

    Changing proxy to test ECS compatibility

--- solvle-front/src/setupProxy.js
+++ solvle-front/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function(app) {
     app.use(createProxyMiddleware('/solvle',
-        { target: 'http://server:8081',
+        { target: 'http://localhost:8081',
             changeOrigin: true
         }
         ));
